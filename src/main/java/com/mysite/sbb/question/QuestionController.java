34d5c1af9c4a7830fe65b8f0eb1bfc105ce76486
humusHS <he2892@humuson.com>
2022-09-29 13:07:43 +0900

    질문 등록, 답변 등록 시 null 체크, alert 추가

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -2,11 +2,15 @@ package com.mysite.sbb.question;
 
 import java.util.List;
 
+import com.mysite.sbb.answer.AnswerForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import lombok.RequiredArgsConstructor;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @Controller
@@ -21,19 +25,27 @@ public class QuestionController {
         model.addAttribute("questionList", questionList);
         return "question_list";
     }
-    @RequestMapping(value = "/detail/{id}")
-    public String detail(Model model, @PathVariable("id") Integer id){
+    @RequestMapping(value = "/detail/{id}") // 검증 과정 때문에 answerForm 파라미터 추가, 값을 넣지 않아서 무작위 값이 들어가있을 것이며 null 체크 회피
+    public String detail(Model model, @PathVariable("id") Integer id, AnswerForm answerForm){
         Question question =  this.questionService.getQuestion(id);
         model.addAttribute("question", question);
         return "question_detail";
     }
-    @GetMapping("/create")
-    public String qustionCreate(){
+    @GetMapping("/create")   //원래는 파라미터 없었지만, form 검증 기능 도입 후 questionForm 추가, 그냥 객체만 생성한 상태이므로 랜덤한 값 들어가 있을 듯 -> null 체크 회피
+    public String qustionCreate(QuestionForm questionForm){
         return "question_form";
     }
+    // 원래는 @RequestParam String subject, @RequestParam String content 로 받아와서 바로 입력하는 방식
+    // 제목과 내용의 null값을 체크하기 위해 @Valid 사용하여 QuestionForm에서 검증
+    // question_form에서 넘어온 subject, content 값이 QuestionForm 객체에 담겨서 전송됨
+    // 전송된 subject, content 값은 스프링 프레임워크의 바인딩 기능을 통해 QuestionForm의 subject, content 값에 자동으로 바인딩된다.
+    //  BindingResult 매개변수는 @Valid 애너테이션으로 인해 검증이 수행된 결과를 의미하는 객체이다.
     @PostMapping("/create")
-    public String qustionCreate(@RequestParam String subject, @RequestParam String content){
-        this.questionService.create(subject, content);
+    public String qustionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            return "question_form";
+        }
+        this.questionService.create(questionForm.getSubject(), questionForm.getContent());
         return "redirect:/question/list"; //질문 저장 후 목록으로 이동
     }
 }
